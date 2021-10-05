a5e6a1264b36842f2614cdd144b3902c6aa26688
Brandon-Valentine <91150793+Brandon-Valentine@users.noreply.github.com>
2021-10-05 17:41:23 -0400

    conditions and input program

---
+++ conditions.py
@@ -0,0 +1,11 @@
+Temp= int(input('Please enter current temperature:'))
+if Temp > 90:
+    print('Wear Shorts')
+elif Temp > 70:
+    print('Short sleeves are fine')
+elif Temp > 50:
+    print('Wear a Jacket')
+elif Temp > 32:
+    print('Wear a heavy coat')
+else:
+    print('Stay inside')
